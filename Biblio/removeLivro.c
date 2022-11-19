5ab5b5f192b711a40c63776c580e2293302e8254
João Victor Amorim <jvoamorim@aluno.puc-rio.br>
2022-11-19 14:49:52 -0300

    finalizada função removeLivro

--- Biblio/removeLivro.c
+++ Biblio/removeLivro.c
@@ -3,9 +3,10 @@
 #include "abreArquivo.h"
 #define TAMLIV 21
 #define TAMEDI 15
+#define NOMEARQAUX "auxiliar.txt"
 
-//retorna 1 quando o livro é cadastrado com sucesso
-//retorna 0 quando livro ja existe na base
+// retorna 1 quando o livro é cadastrado com sucesso
+// retorna 0 quando livro ja existe na base
 int removeLivro(char *nomeArq, int idRemover)
 {
     // buscar livro para saber se ja existe
@@ -13,31 +14,49 @@ int removeLivro(char *nomeArq, int idRemover)
     FILE *arq;
     FILE *arqAux;
     arq = openFile(nomeArq, "r");
-    arqAux = openFile("auxiliar.txt","a");
+    arqAux = openFile(NOMEARQAUX, "a");
     int idArq;
     char nomeLivro[TAMLIV], editora[TAMEDI];
     int id, alugado, matr;
-    while (!feof(arq))
+    while (fscanf(arq, "%d , %[^,] , %[^,] , %d , %d", &idArq, nomeLivro, editora, &alugado, &matr) != EOF)
     {
-        //ler todo o arquivo se n for a linha a ser deletada, escreve
-        //se for a linha a ser deletada não 
-        if (fscanf(arq, "%[^,]", idArq) != 1)
-            break;
+        // ler todo o arquivo se n for a linha a ser deletada, escreve
+        // se for a linha a ser deletada não
         if (idArq != idRemover)
         {
-            fscanf(arqAux,"%d , %[^,] , %[^,] , %d , %d",&id, nomeLivro, editora, &alugado, &matr);
-            //escreve no auxiliar
-           fprintf(arqAux, "%d, %s, %s, %d, %d", id, nomeLivro, editora, alugado, matr);
+            // escreve no auxiliar
+            fprintf(arqAux, "%d, %s, %s, %d, %d", id, nomeLivro, editora, alugado, matr);
+        }
+
+    }
+    fclose(arq);
+    fclose(arqAux);
+
+    arq = openFile(nomeArq, "w");
+    arqAux = openFile(NOMEARQAUX,"r");
+
+    while (!feof(arqAux))
+    {
+        //le do arquivo auxiliar e escreve no principal
+        fscanf(arqAux, "%d , %[^,] , %[^,] , %d , %d", &idArq, nomeLivro, editora, &alugado, &matr);
+        fprintf(arq,"%d, %s, %s, %d, %d", id, nomeLivro, editora, alugado, matr);
+    }
+
+    fclose(arq);
+    fclose(arqAux);
+
+    arq = openFile(nomeArq, "r");
+
+    while (fscanf(arq, "%d , %[^,] , %[^,] , %d , %d", &idArq, nomeLivro, editora, &alugado, &matr) != EOF)
+    {
+        // ler todo o arquivo principal para verificar se o id foi excluido corretamente
+        //se nao foi removido retorna 0
+        if (idArq == idRemover)
+        {
+            return 0;
         }
     }
     fclose(arq);
-    if (comp == 1)
-    {
-        //remove o livro 
-        int alugado = 0;
-        arq = openFile(nomeArq, "a");
-        fprintf(arq, "%d, %s, %s, %d, %d", id, nomeLivro, editora, alugado, matr);
-        fclose(arq);
-    }
+    
     return 1;
 }
